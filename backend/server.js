a9be71950a361a79c5490f107fc2392f91ace2f5
Nguyen Han Manh Kiet <tieulyngu98@gmail.com>
2022-07-09 23:42:27 +0700

    update port

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const userRouter = require("./routes/user.route");
 
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
